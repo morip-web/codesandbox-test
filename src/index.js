14db4e326d7c25f24eb4cbafd09c2432de14cfb5
もりぴ <66156635+morip-web@users.noreply.github.com>
2021-02-24 13:23:52 +0900

    デフォルト値・スプレッド構文

--- src/index.js
+++ src/index.js
@@ -67,3 +67,40 @@ const myProfile2 = ["morip", 29];
 const [name2, age2] = myProfile2;
 const message4 = `名前は${name2}です。年齢は${age2}歳です。`;
 console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+
+const sayHello = (name3 = "Gust") => console.log(`こんにちは${name3}さん！`);
+sayHello();
+
+/**
+ * スプレッド構文
+ */
+
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num3, num4, ...arr3] = arr2;
+console.log(num3);
+console.log(num4);
+console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
