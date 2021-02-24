c11de427c8a663f9b80c4721647a9821f330c48c
もりぴ <66156635+morip-web@users.noreply.github.com>
2021-02-24 10:12:42 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
  * テンプレート文字列
  */
 
-const name = "morip";
-const age = 50;
+const NAME = "morip";
+const AGE = 50;
 
 // 「私の名前はmoripです。年齢は50歳です。」
-const message1 = `私の名前は${name}です。年齢は${age}歳です。`;
+const message1 = `私の名前は${NAME}です。年齢は${AGE}歳です。`;
 console.log(message1);
 
 /**
@@ -43,3 +43,27 @@ console.log(func2("morip3"));
 
 const func4 = (num1, num2) => num1 + num2;
 console.log(func4(3, 4));
+
+/**
+ * 分割代入
+ */
+
+// 従来の記法
+// const myProfile = {
+//   name: "morip",
+//   age: 50
+// };
+
+// const message2 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message2);
+
+// 分割代入による記法
+// const {name, age} = myProfile;
+// const message3 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message3);
+
+// 配列から
+const myProfile2 = ["morip", 29];
+const [name2, age2] = myProfile2;
+const message4 = `名前は${name2}です。年齢は${age2}歳です。`;
+console.log(message4);
